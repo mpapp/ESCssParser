f7e0cbbf2232ff3a026f0e1d370b7046140f0ba3
mz2 <matias.piipari@gmail.com>
2016-03-09 00:08:31 +0000

    Adds a Carthage buildable OSX framework target.

---
+++ ESCssParserKit/ESCssParserKit/ESCssParserKit.h
@@ -0,0 +1,20 @@
+//
+//  ESCssParserKit.h
+//  ESCssParserKit
+//
+//  Created by Matias Piipari on 09/03/2016.
+//  Copyright © 2016 Manuscripts.app Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ESCssParserKit.
+FOUNDATION_EXPORT double ESCssParserKitVersionNumber;
+
+//! Project version string for ESCssParserKit.
+FOUNDATION_EXPORT const unsigned char ESCssParserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESCssParserKit/PublicHeader.h>
+
+#import "ESCssParser.h"
+#import "ESCssTokens.h"
